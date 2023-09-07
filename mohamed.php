ffb7f0bcaa7e0d5284ec7ecd8eae95529e271147
m22005188 <mohamed.mesli@etu.univ-amu.fr>
2023-09-07 11:56:20 +0200

    .

---
+++ mohamed.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device width, initial-scale=1">
+    <title>Form Login and Register</title>
+</head>
+</html>
+<div class="signup">
+    <form>
+        <h1>Sign up</h1>
+        <h4>Join Woap !</h4>
+        <p>Nom</p>
+        <input type="text" name="Nom">
+        <p>Prenom</p>
+        <input type="text" name="Prenom">
+        <p>email</p>
+        <input type="email" name="email">
+        <p>mot de passe</p>
+        <input type="password" name="password">
+        <p>Répetez votre mot de passe</p>
+        <input type="password" name="repeatpassword"><br><br>
+        <input type="submit" name="submit" value="Sign up">
+        <form>
+            <p>By clicking the Sign up button, you agree to our <br>
+                <a href="">Terms and Condition</a> and <a href="#"> Policy Privacy </a>
+            </p>
+
+
+
+</div>
