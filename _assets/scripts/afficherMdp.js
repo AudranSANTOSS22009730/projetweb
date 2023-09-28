a4ae202247fae15ec945bd6f0d8165931eaa3dff
SoudaniLyes <125369497+SOUDANI-REGUIG-Lyes-yacine-2225082a@users.noreply.github.com>
2023-09-28 10:44:25 +0200

    Add files via upload

---
+++ _assets/scripts/afficherMdp.js
@@ -0,0 +1,17 @@
+<!-- JavaScript pour afficher/masquer le mot de passe -->
+/*
+<script>
+    const passwordField = document.getElementById('password');
+    const showPasswordCheckbox = document.getElementById('showPassword');
+    const passwordRetypeField = document.getElementById('password_retype');
+    const showPasswordRetypeCheckbox = document.getElementById('showPasswordRetype');
+
+    showPasswordCheckbox.addEventListener('change', function () {
+    passwordField.type = this.checked ? 'text' : 'password';
+});
+
+    showPasswordRetypeCheckbox.addEventListener('change', function () {
+    passwordRetypeField.type = this.checked ? 'text' : 'password';
+});
+</script>
+*/
