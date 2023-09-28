82240753d5f2029f4c3917eee0851faed0362dfd
d22011835 <alexis.delin@etu.univ-amu.fr>
2023-09-28 12:08:02 +0200

    modifiaction temporaire mdp oublié

--- mdpOublie.php
+++ mdpOublie.php
@@ -45,15 +45,30 @@
     <form method="post">
         <h2 class="text-center">Mot de passe oublié</h2>
         <div class="form-group">
-            <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
+            <input type="email" name="email_oublie" id="email_oublie" class="form-control" placeholder="Email" required="required" autocomplete="off">
         </div>
         <?php
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if(!empty($_POST["email_oublie"])){
+            $query="select * from user where email=:mail";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute(["mail"=>$_POST["email_oublie"]]);
+            $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
+            var_dump($user_data);
+            if (!$user_data) {
+                $msg = "Utilisateur non existant";
+                $response=[
+                    "value"=>false,
+                    "msg"=>$msg
+                ];
+            }
+        }
+
+        /*if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $to_email = $_POST['email'];
 
             if (filter_var($to_email, FILTER_VALIDATE_EMAIL)) {
                 $subject = 'Réinitialisation de mot de passe';
-                $message = 'Cliquez sur le lien suivant pour réinitialiser votre mot de passe : ';
+                $message = 'Cliquez sur le lien suivant pour reinitialiser votre mot de passe : ';
                 $headers = 'From: wap@alwaysdata.net';
 
                 mail($to_email, $subject, $message, $headers);
@@ -61,7 +76,7 @@
             } else {
                 echo '<div class="alert alert-danger">Adresse e-mail invalide. Veuillez entrer une adresse e-mail valide.</div>';
             }
-        }
+        }*/
         ?>
         <div class="form-group">
             <button type="submit" class="btn btn-primary btn-block">Suivant</button>
