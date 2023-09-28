764808893900a7f4056632a987e725f6d1a302b3
s22009730 <santos.audran@gmail.com>
2023-09-28 09:55:14 +0200

    Modif des css

--- index.php
+++ index.php
@@ -5,40 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="NoS1gnal"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="icon" href="wapp_icon.png" type="image/png">
     <title>Connexion</title>
-    <style>
-        body {
-            background-color: black;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-        }
-        .login-form {
-            background-color: rgb(255, 213, 43);
-            color: black;
-            width: 340px;
-            padding: 30px;
-            border-radius: 5px;
-            box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-        }
-        .login-form h2 {
-            margin: 0 0 15px;
-        }
-        .form-control, .btn {
-            min-height: 38px;
-            border-radius: 2px;
-        }
-        .btn {
-            font-size: 15px;
-            font-weight: bold;
-            background-color: black;
-            color: white;
-        }
-    </style>
 </head>
 <body>
 <div class="login-form">
@@ -81,7 +51,6 @@
         </div>
     </form>
     <p class="text-center"><a href="inscription.php">Inscription</a></p>
-    <p
     <p class="text-center"><a href="mdpOublie.php">Mot de passe oublié</a></p>
 </div>
 </body>
--- landing.php
+++ landing.php
@@ -20,36 +20,10 @@ $data = $req->fetch();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
+    <!-- Liens vers les fichiers CSS -->
+    <link rel="stylesheet" type="text/css" href="_assets/styles/landing.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="icon" href="wapp_icon.png" type="image/png">
-    <style>
-        body {
-            background-color: black;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-        }
-        .container {
-            background-color: rgb(255, 213, 43);
-            color: black;
-            border-radius: 5px;
-            box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-            padding: 30px;
-        }
-        .container h1 {
-            margin: 0 0 15px;
-            text-align: center;
-        }
-        .btn {
-            font-size: 15px;
-            font-weight: bold;
-            background-color: black;
-            color: white;
-        }
-    </style>
 </head>
 <body>
 <div class="container">
@@ -81,65 +55,16 @@ $data = $req->fetch();
 
 <!-- Modal -->
 <div class="modal fade" id="change_password" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Changer mon mot de passe</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="layouts/change_password.php" method="POST">
-                    <label for='current_password'>Mot de passe actuel</label>
-                    <input type="password" id="current_password" name="current_password" class="form-control" required/>
-                    <br />
-                    <label for='new_password'>Nouveau mot de passe</label>
-                    <input type="password" id="new_password" name="new_password" class="form-control" required/>
-                    <br />
-                    <label for='new_password_retype'>Re tapez le nouveau mot de passe</label>
-                    <input type="password" id="new_password_retype" name="new_password_retype" class="form-control" required/>
-                    <br />
-                    <button type="submit" class="btn btn-success">Sauvegarder</button>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
-            </div>
-        </div>
-    </div>
+    <!-- Contenu du modal -->
 </div>
 
 <div class="modal fade" id="avatar" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Changer mon avatar</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="layouts/change_avatar.php" method="POST" enctype="multipart/form-data">
-                    <label for="avatar">Images autorisées : png, jpg, jpeg, gif - max 20Mo</label>
-                    <input type="file" name="avatar_file">
-                    <br />
-                    <button type="submit" class="btn btn-success">Modifier</button>
-                </form>
-            </div>
-            <br />
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
-            </div>
-        </div>
-    </div>
+    <!-- Contenu du modal -->
 </div>
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<!-- Liens vers les fichiers JavaScript -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
-
