adf6aeef5a8c31428b5e22b9bc4a07fed0151e0f
s22009730 <santos.audran@gmail.com>
2023-10-06 11:02:05 +0200

    mvc pour la connexion et l'acceuil

--- modules/blog/views/acceuil_view.php
+++ modules/blog/views/acceuil_view.php
@@ -9,61 +9,65 @@
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
 <body>
-<div class="navigation">
-    <ul>
-        <li class="list active">
-            <a href="#">
+<div class="wrapper">
+    <div class="navigation">
+        <ul>
+            <li class="list active">
+                <a href="#">
                     <span class="icon">
                         <ion-icon name="happy-sharp"></ion-icon>
                     </span>
-                <span class="text">Amis</span>
-            </a>
-        </li>
-        <li class="list">
-            <a href="#">
+                    <span class="text">Amis</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
                     <span class="icon">
                         <ion-icon name="home-outline"></ion-icon>
                     </span>
-                <span class="text">Home</span>
-            </a>
-        </li>
-        <li class="list">
-            <a href="#">
+                    <span class="text">Home</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
+                    <span class="icon">
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                    </span>
+                    <span class="text">Messages</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
                     <span class="icon">
                         <ion-icon name="globe-sharp"></ion-icon>
                     </span>
-                <span class="text">Groupes</span>
-            </a>
-        </li>
-        <li class="list">
-            <a href="#">
-                    <span class="icon">
-                        <ion-icon name="globe-sharp"></ion-icon>
-                    </span>
-                <span class="text">Photos</span>
-            </a>
-        </li>
-        <li class="list">
-            <a href="#">
+                    <span class="text">Photos</span>
+                </a>
+            </li>
+            <li class="list">
+                <a href="#">
                     <span class="icon">
                         <ion-icon name="notifications-outline"></ion-icon>
                     </span>
-                <span class="text">Notifications</span>
-            </a>
-        </li>
+                    <span class="text">Notifications</span>
+                </a>
+            </li>
+        </ul>
         <div class="indicator"></div>
-    </ul>
+    </div>
 </div>
 
 <script>
     const list = document.querySelectorAll('.list');
-    function activeLink(){
-        list.forEach((item) =>
-        item.classList.remove('active');
-        this.classList.add('active')
+    function activeLink() {
+        list.forEach((item) => {
+            item.classList.remove('active');
+        });
+        this.classList.add('active');
     }
-    list.forEach((item)
-    item.addEventListener('click', activeLink));
+    list.forEach((item) => {
+        item.addEventListener('click', activeLink);
+    });
 </script>
 
 </body>
